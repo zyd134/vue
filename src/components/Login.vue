22b643c2e1f4da8f0711ca62a6dcb4ff1ba00f42
zyd134 <2602828321@qq.com>
2020-09-18 17:25:29 +0800

    第一次提交代码

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,7 +2,7 @@
   <div>
     用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
     <br><br>
-    密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
+    密 码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
     <br><br>
     <button v-on:click="login">登录</button>
   </div>
